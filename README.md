a1f17819f8b5813bf9695c85c5f78a58863eaec2
Marcin Cylke <marcin.cylke@xxx.pl>
2019-11-14 09:44:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Gitlab Express
+--------
+
+To use it, copy `config_sample.js` to `config.js` and change
+configuration to fit your projects and Gitlab instance. Generate private
+access token via Gitlab UI.
---
+++ config_sample.js
@@ -0,0 +1,8 @@
+var projectUris = [
+        "projectA/a",
+        "projectB/b",
+    ];
+
+var urlTemplate = "https://gitlab.example.com/api/v4/projects/XXX/pipelines";
+
+var PrivateToken = "some secret"; // generated using https://gitlab.example.com/profile/personal_access_tokens
